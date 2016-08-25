605385a6bf4513c8b2d0b507318e13784d859096
Luis Benet <benet@fis.unam.mx>
2016-08-25 18:37:55 -0500

    Add .gitignore, put functions as initial parameter and other scattered improvements
    I went back to the convention that the functions representing the equations
    of motion appear as the first parameter in some calling functions.

--- src/taylor_integration_methods.jl
+++ src/taylor_integration_methods.jl
@@ -1,20 +1,20 @@
 # This file is part of the TaylorIntegration.jl package; MIT licensed
 
 doc"""
-    jetcoeffs!(t, x, f)
+    jetcoeffs!(f, t, x)
 
 Returns an updated `x` using the recursion relation of the
-derivatives from the ODE $\dot{x}=f(t,x)$.
+derivatives from the ODE $\dot{x}=dx/dt=f(t,x)$.
 
-`f` is the function describing the RHS of the ODE, `x` is
-a `TaylorSeries.Taylor1{T}` containing the Taylor expansion
-of the dependent variable of the ODE and `t` is the independent
+`f` is the function defining the RHS of the ODE, `x` is a `Taylor1{T}`
+or a vector of that type, containing the Taylor expansion
+of the dependent variables of the ODE and `t` is the independent
 variable.
 Initially, `x` contains only the 0-th order Taylor coefficients of
 the current system state (the initial conditions), and `jetcoeffs!`
-fills recursively the high-order derivates back into `x`.
+computes recursively the high-order derivates back into `x`.
 """
-function jetcoeffs!{T<:Number}(t0::T, x::Taylor1{T}, eqsdiff)
+function jetcoeffs!{T<:Number}(eqsdiff, t0::T, x::Taylor1{T})
     order = x.order
     for ord in 1:order
         ordnext = ord+1
@@ -32,28 +32,15 @@ function jetcoeffs!{T<:Number}(t0::T, x::Taylor1{T}, eqsdiff)
     nothing
 end
 
-doc"""
-    jetcoeffs!(t, x, f)
-
-Returns an updated `x` using the recursion relation of the
-derivatives from the ODE $\dot{x}=f(t,x)$.
-
-`f` is the function describing the RHS of the ODE, `x` is
-a vector of `TaylorSeries.Taylor1{T}` containing the Taylor expansions
-of the dependent variables of the ODE.
-Initially, `x` contains only the 0-th order Taylor coefficients of
-the current system state (the initial conditions), and `jetcoeffs!`
-fills recursively the high-order derivates back into `x`.
-"""
-function jetcoeffs!{T<:Number}(t0::T, x::Vector{Taylor1{T}}, eqsdiff)
+function jetcoeffs!{T<:Number}(eqsdiff, t0::T, x::Vector{Taylor1{T}})
     order = x[1].order
     xaux = similar(x)
     for ord in 1:order
         ordnext = ord+1
 
         # Set `xaux`, auxiliary vector of Taylor1 to order `ord`
-        for j in eachindex(x)
-            @inbounds xaux[j] = Taylor1( x[j].coeffs[1:ord] )
+        @inbounds for j in eachindex(x)
+            xaux[j] = Taylor1( x[j].coeffs[1:ord] )
         end
 
         # Equations of motion
@@ -61,8 +48,8 @@ function jetcoeffs!{T<:Number}(t0::T, x::Vector{Taylor1{T}}, eqsdiff)
         F = eqsdiff(t0, xaux)
 
         # Recursion relations
-        for j in eachindex(x)
-            @inbounds x[j].coeffs[ordnext] = F[j].coeffs[ord]/ord
+        @inbounds for j in eachindex(x)
+            x[j].coeffs[ordnext] = F[j].coeffs[ord]/ord
         end
     end
     nothing
@@ -72,7 +59,7 @@ end
 doc"""
     stepsize(x, epsilon)
 
-Returns a time-step for a `x::TaylorSeries.Taylor1{T}` using a
+Returns a time-step for a `x::Taylor1{T}` using a
 prescribed absolute tolerance `epsilon`.
 """
 function stepsize{T<:Number}(x::Taylor1{T}, epsilon::T)
@@ -89,7 +76,7 @@ end
 doc"""
     stepsize(q, epsilon)
 
-Returns the minimum time-step for `q::Array{TaylorSeries.Taylor1{T},1}`,
+Returns the minimum time-step for `q::Array{Taylor1{T},1}`,
 using a prescribed absolute tolerance `epsilon`.
 """
 function stepsize{T<:Number}(q::Array{Taylor1{T},1}, epsilon::T)
@@ -110,18 +97,31 @@ the dependent variables of an ODE, at *time* δt.
 """
 function evaluate{T<:Number}(x::Array{Taylor1{T},1}, δt::T)
     xnew = Array{T,1}( length(x) )
-    for i in eachindex(x)
-        @inbounds xnew[i] = evaluate( x[i], δt )
-    end
+    evaluate!(x, δt, xnew)
     return xnew
+end
+
+doc"""
+    evaluate!(x, δt, x0)
+
+Evaluates each element of `x::Array{Taylor1{T},1}`, representing
+the Taylor expansion for the dependent variables of an ODE at
+*time* δt; it updates the vector `x0` with the computed values.
+"""
+function evaluate!{T<:Number}(x::Array{Taylor1{T},1}, δt::T, x0::Array{T,1})
+    @assert length(x) == length(x0)
+    @inbounds for i in eachindex(x)
+        x0[i] = evaluate( x[i], δt )
+    end
+    nothing
 end
 
 
 doc"""
-    taylorstep!(t0, x0, abs_tol, order, f)
+    taylorstep(f, t0, x0, abs_tol, order)
 
-Compute one-step Taylor integration for the ODE $\dot{x}=f(t, x)$
-with initial condition $x(t_0)=x0$, updating `x0` and returning
+Compute one-step Taylor integration for the ODE $\dot{x}=dx/dt=f(t, x)$
+with initial condition $x(t_0)=x0$, returning `x0` and
 the step-size of the integration carried out.
 
 Here, `x0` is the initial (and updated) dependent variable, `order`
@@ -129,31 +129,43 @@ is the degree used for the `Taylor1` polynomials during the integration
 and `abs_tol` is the absolute tolerance used to determine the time step
 of the integration.
 """
-function taylorstep!{T<:Number}(t0::T, x0::T, order::Int, abs_tol::T, f)
+function taylorstep{T<:Number}(f, t0::T, x0::T, order::Int, abs_tol::T)
     # Inizialize the Taylor1 expansions
     xT = Taylor1( x0, order )
     # Compute the Taylor coefficients
-    jetcoeffs!(t0, xT, f)
+    jetcoeffs!(f, t0, xT)
     # Compute the step-size of the integration using `abs_tol`
     δt = stepsize(xT, abs_tol)
     x0 = evaluate(xT, δt)
-    t0 += δt
-    return nothing
+    return δt, x0
 end
-function taylorstep!{T<:Number}(t0::T, x0::Array{T,1}, order::Int, abs_tol::T, f)
+
+doc"""
+    taylorstep!(f, t0, x0, abs_tol, order)
+
+Compute one-step Taylor integration for the ODE $\dot{x}=dx/dt=f(t, x)$
+with initial conditions $x(t_0)=x0$, a vector of type T, updating `x0`
+and returning the step-size of the integration carried out.
+
+Here, `x0` is the initial (and updated) dependent variables, `order`
+is the degree used for the `Taylor1` polynomials during the integration
+and `abs_tol` is the absolute tolerance used to determine the time step
+of the integration.
+"""
+function taylorstep!{T<:Number}(f, t0::T, x0::Array{T,1}, order::Int, abs_tol::T)
     # Inizialize the vector of Taylor1 expansions
     xT = Array{Taylor1{T},1}(length(x0))
     for i in eachindex(x0)
         @inbounds xT[i] = Taylor1( x0[i], order )
     end
     # Compute the Taylor coefficients
-    jetcoeffs!(xT, f)
+    jetcoeffs!(f, t0, xT)
     # Compute the step-size of the integration using `abs_tol`
     δt = stepsize(xT, abs_tol)
-    x0 = evaluate(xT, δt)
-    t0 += δt
-    return nothing
+    evaluate!(xT, δt, x0)
+    return δt
 end
+
 
 doc"""
     taylorinteg(x0, t0, tmax, order, abs_tol, f[, maxsteps::Int=500])
@@ -170,15 +182,15 @@ The integrator uses polynomial expansions on the independent variable
 of order `order` and the parameter `abs_tol` serves to define the
 time step using the last two Taylor coefficients of the expansions.
 """
-function taylorinteg{T<:Number}(x0::T, t0::T, t_max::T,
-        order::Int, abs_tol::T, f, maxsteps::Int=500)
+function taylorinteg{T<:Number}(f, x0::T, t0::T, t_max::T,
+        order::Int, abs_tol::T, maxsteps::Int=500)
 
     tv = [t0]
     xv = [x0]
     nsteps = 0
     while (t0 < t_max) && (nsteps < maxsteps)
-        taylorstep!(t0, x0, order, abs_tol, f)
-        # t += dt
+        δt, x0 = taylorstep(f, t0, x0, order, abs_tol)
+        t0 += δt
         push!(tv, t0)
         push!(xv, x0)
         nsteps += 1
@@ -187,15 +199,16 @@ function taylorinteg{T<:Number}(x0::T, t0::T, t_max::T,
     return tv, xv
 end
 
-function taylorinteg{T<:Number}(x0::Array{T,1}, t0::T, t_max::T,
-    order::Int, abs_tol::T, f, maxsteps::Int=500)
+function taylorinteg{T<:Number}(f, x0::Array{T,1}, t0::T, t_max::T,
+        order::Int, abs_tol::T, maxsteps::Int=500)
 
     tv = [t0]
     xv = Array{typeof(x0),1}()
     push!(xv, x0)
-
+    nsteps = 0
     while (t0 < t_max) && (nsteps < maxsteps)
-        taylorstep!(t0, x0, order, abs_tol, f)
+        δt = taylorstep!(f, t0, x0, order, abs_tol)
+        t0 += δt
         push!(tv, t0)
         push!(xv, x0)
         nsteps += 1
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,7 +16,7 @@ facts("Tests: dot{x}=x^2, x(0) = 1") do
     t0 = 0.0
     x0 = 1.0
     x0T = Taylor1(x0, _order)
-    jetcoeffs!(t0, x0T, eqs_mov)
+    jetcoeffs!(eqs_mov, t0, x0T)
     @fact x0T.coeffs[end] == 1.0 --> true
     δt = _abs_tol^inv(_order-1)
     @fact stepsize(x0T, _abs_tol) == δt --> true
@@ -30,12 +30,11 @@ facts("Tests: dot{x}=x.^2, x(0) = 3") do
     q0 = [3.0]
     q0T = Array{Taylor1{Float64},1}(1)
     q0T[1] = Taylor1(q0[1], _order)
-    jetcoeffs!(t0, q0T, eqs_mov)
+    jetcoeffs!(eqs_mov, t0, q0T)
     @fact q0T[1].coeffs[end] == 3.0^(_order+1) --> true
     δt = (_abs_tol/q0T[1].coeffs[end-1])^inv(_order-1)
     @fact stepsize(q0T, _abs_tol) == δt --> true
     @fact evaluate(q0T, δt)[1] - 3.0/(1.0-3.0*δt) < eps(1.0) --> true
-
 end
 
 exitstatus()
