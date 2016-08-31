8aa3d388ec16dd17ec1fb3ecf269959133b86c3d
Luis Benet <benet@fis.unam.mx>
2016-08-31 17:06:46 -0500

    Fix typo in a docstring

--- src/taylor_integration_methods.jl
+++ src/taylor_integration_methods.jl
@@ -242,7 +242,7 @@ doc"""
 This is a general-purpose Taylor integrator for the explicit ODE
 $\dot{x}=f(x)$ with initial condition specified by `x0` at time `t0`.
 It returns a vector with the values of time (independent variable),
-and a vector (of of type `typeof(x0)`) with the computed values of
+and a vector (of type `typeof(x0)`) with the computed values of
 the dependent variables. The integration stops when time
 is larger than `tmax`, or the number of saved steps is larger
 than `maxsteps`.
