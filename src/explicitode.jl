9c002b19ce99bab3d202e4b4bbd015e6e03fe375
Jorge Perez <jperez@icf.unam.mx>
2016-10-18 10:34:07 -0500

    Fix array promotion

--- src/explicitode.jl
+++ src/explicitode.jl
@@ -291,8 +291,8 @@ function taylorinteg{S<:Number, T<:Number, U<:Number, V<:Number}(f,
 
     #promote to common type before integrating:
     elq0, t0, tmax, abstol = promote(q0[1], t0, tmax, abstol)
-    #convert the elements of q0 to the common type:
-    q0 = map(x->convert(typeof(elq0), x), q0)
+    #convert the elements of q0 to the common, promoted type:
+    q0 = convert(Array{typeof(elq0)}, q0)
 
     taylorinteg(f, q0, t0, tmax, order, abstol, maxsteps=maxsteps)
 
