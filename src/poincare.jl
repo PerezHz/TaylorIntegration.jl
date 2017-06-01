f4a00d9c994d00f93346aee436e676d74ba27df3
Jorge Perez <jperez@icf.unam.mx>
2017-06-01 16:40:28 -0500

    Change NR iterations 4->5

--- src/poincare.jl
+++ src/poincare.jl
@@ -67,7 +67,7 @@ function poincare{T<:Number}(f!, g, q0::Array{T,1}, t0::T, tmax::T,
             # println(" * dt_nr0 = ", dt_nr)
             evaluate!(x_g_Dg_D2g[2dof+1:2dof+2], dt_nr, view(x_g_Dg_D2g_val,2dof+1:2dof+2))
 
-            for i in 1:4
+            for i in 1:5
                 dt_nr = dt_nr-x_g_Dg_D2g_val[2dof+1]/x_g_Dg_D2g_val[2dof+2]
                 # println("i = ", i, ", dt_nr = ", dt_nr)
                 evaluate!(x_g_Dg_D2g[2dof+1:2dof+2], dt_nr, view(x_g_Dg_D2g_val,2dof+1:2dof+2))
@@ -178,7 +178,7 @@ function poincare{T<:Number}(f!, g, q0::Array{TaylorN{T},1}, t0::T, tmax::T,
             # println(" * dt_nr0 = ", dt_nr)
             evaluate!(x_g_Dg_D2g[2dof+1:2dof+2], dt_nr, view(x_g_Dg_D2g_val,2dof+1:2dof+2))
 
-            for i in 1:4
+            for i in 1:5
                 dt_nr = dt_nr-x_g_Dg_D2g_val[2dof+1]/x_g_Dg_D2g_val[2dof+2]
                 # println("i = ", i, ", dt_nr = ", dt_nr)
                 evaluate!(x_g_Dg_D2g[2dof+1:2dof+2], dt_nr, view(x_g_Dg_D2g_val,2dof+1:2dof+2))
@@ -287,7 +287,7 @@ function poincare2{T<:Number}(f!, g, q0::Array{T,1}, t0::T, tmax::T,
             # println(" * dt_nr0 = ", dt_nr)
             evaluate!(x_g_Dg_D2g[2dof+1:2dof+2], dt_nr, view(x_g_Dg_D2g_val,2dof+1:2dof+2))
 
-            for i in 1:4
+            for i in 1:5
                 dt_nr = dt_nr-x_g_Dg_D2g_val[2dof+1]/x_g_Dg_D2g_val[2dof+2]
                 # println("i = ", i, ", dt_nr = ", dt_nr)
                 evaluate!(x_g_Dg_D2g[2dof+1:2dof+2], dt_nr, view(x_g_Dg_D2g_val,2dof+1:2dof+2))
