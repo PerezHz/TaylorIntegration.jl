29025c971cb1006fbf204514d2010f5c69dbd4c3
Luis Benet <benet@fis.unam.mx>
2017-08-06 16:43:19 -0500

    Add constant_term consistently

--- src/parse_eqs.jl
+++ src/parse_eqs.jl
@@ -1,6 +1,7 @@
 # This file is part of the TaylorIntegration.jl package; MIT licensed
 
-using TaylorSeries: constant_term, _dict_unary_calls, _dict_binary_calls
+# Load necessary components of TaylorSeries, MacroTools and Espresso
+using TaylorSeries: _dict_unary_calls, _dict_binary_calls
 
 # Simple access to all internal mutating functions from TaylorSeries
 for kk in keys(_dict_unary_calls)
@@ -16,11 +17,11 @@ end
 
 using MacroTools: @capture, shortdef
 
-using Espresso: subs, ExGraph, to_expr, sanitize, genname, rewrite
+using Espresso: subs, ExGraph, to_expr, sanitize, genname
 
 
 
-
+# Define some constants for the newly (parsed) functions
 const _HEAD_PARSEDFN_SCALAR = sanitize(:(
 function parsed_jetcoeffs!{T<:Number}(__t0::T, __x::Taylor1{T}, __vT::Vector{T})
     order = __x.order
@@ -28,35 +29,25 @@ function parsed_jetcoeffs!{T<:Number}(__t0::T, __x::Taylor1{T}, __vT::Vector{T})
 end)
 );
 
-const _LOOP_PARSEDFN_SCALAR = sanitize(:(
-    for ord in 1:order
-        ordnext = ord+1
-    end
-    # Symbol("_ret_var")
-    )
-);
-
 const _HEAD_PARSEDFN_VECTOR = sanitize(:(
-function parsed_jetcoeffs!{T<:Number}(__t0::T, __x::Vector{Taylor1{T}}, __dx::Vector{Taylor1{T}},
-            __xaux::Vector{Taylor1{T}}, __vT::Vector{T})
+function parsed_jetcoeffs!{T<:Number}(__t0::T, __x::Vector{Taylor1{T}},
+        __dx::Vector{Taylor1{T}},
+        __xaux::Vector{Taylor1{T}}, __vT::Vector{T})
 
     order = __x[1].order
     __vT[1] = __t0
 end)
 );
 
-const _LOOP_PARSEDFN_VECTOR = sanitize(:(
+const _LOOP_PARSEDFN = sanitize(:(
     for ord in 1:order
         ordnext = ord+1
     end
-    # nothing
     )
 );
 
 
-
-
-function _head_newfunct(fn, fnargs)
+function _nfnhead(fn, fnargs)
 
     # Construct common elements of the new expression
     if length(fnargs) == 2
@@ -68,104 +59,125 @@ function _head_newfunct(fn, fnargs)
         "Wrong number of arguments in the definition of the function $fn"))
     end
 
-    # Rename the new function
+    # Rename the new function; equivalent to:
     # newfunction.args[1].args[1].args[1] = Symbol(fn, "_parsed_jetcoeffs!")
     newfunction = subs( newfunction,
-        Dict(:(parsed_jetcoeffs!) => Symbol(fn, "_parsed_jetcoeffs!")) )
+        Dict(:(parsed_jetcoeffs!) => Symbol(fn, "_parsed!")) )
 
-
-    # Adapt body to insert new lines
-    # newfunction.args[2].args[3].args[2] = Expr(:block, ordnext)
-    # newfunction = subs(newfunction,
-    #     Dict(:(ordnext = ord+1) => Expr(:block, :(ordnext = ord+1))))
-
-    newfunction
+    return newfunction
 end
 
 
-function _replace_exprs!(aaa::Expr, aavar::Symbol, v_vars)
+function _replacevars!(fnold::Expr, newvar::Symbol, v_vars)
 
-    if length(aaa.args) == 2
+    ll = length(fnold.args)
 
+    if ll == 2
         # Unary call
-        @assert(in(aaa.args[1], keys(_dict_unary_calls)))
-        fnexpr, aux_fnexpr = _dict_unary_calls[aaa.args[1]]
-        fnexpr = subs(fnexpr, Dict(:_res => aavar,
-            :_arg1 => aaa.args[2], :_k => :ord))
+        @assert(in(fnold.args[1], keys(_dict_unary_calls)))
+
+        # Replacements
+        fnexpr, def_fnexpr, aux_fnexpr = _dict_unary_calls[fnold.args[1]]
+        fnexpr = subs(fnexpr, Dict(:_res => newvar,
+            :_arg1 => fnold.args[2], :_k => :ord))
+        def_fnexpr = subs(def_fnexpr, Dict(:_res => newvar,
+            :_arg1 => :(constant_term($(fnold.args[2]))), :_k => :ord))
+
         # Auxiliary expression
         if aux_fnexpr.head != :nothing
             newvar = genname()
             aux_fnexpr = subs(aux_fnexpr,
-                Dict(:_arg1 => aaa.args[2], :_aux => newvar))
-            push!(v_vars, newvar)
+                Dict(:_arg1 => :(constant_term($(fnold.args[2]))), :_aux => newvar))
             fnexpr = subs(fnexpr, Dict(:_aux => newvar))
+            push!(v_vars, newvar)
         end
-    elseif length(aaa.args) == 3
-
+    elseif ll == 3
         # Binary call; no auxiliary expressions needed
-        @assert(in(aaa.args[1], keys(_dict_binary_calls)))
-        fnexpr, aux_fnexpr = _dict_binary_calls[aaa.args[1]]
-        fnexpr = subs(fnexpr, Dict(:_res => aavar,
-            :_arg1 => aaa.args[2], :_arg2 => aaa.args[3],
+        @assert(in(fnold.args[1], keys(_dict_binary_calls)))
+
+        # Replacements
+        fnexpr, def_fnexpr, aux_fnexpr = _dict_binary_calls[fnold.args[1]]
+        fnexpr = subs(fnexpr, Dict(:_res => newvar,
+            :_arg1 => fnold.args[2], :_arg2 => fnold.args[3],
+            :_k => :ord))
+        def_fnexpr = subs(def_fnexpr, Dict(:_res => newvar,
+            :_arg1 => :(constant_term($(fnold.args[2]))),
+            :_arg2 => :(constant_term($(fnold.args[3]))),
             :_k => :ord))
     else
-        throw(ArgumentError(":call is not unary or binary"))
+        throw(ArgumentError("`:call` is not unary or binary; use parenthesis!"))
     end
 
-    push!(v_vars, aavar)
-
-    return fnexpr, aux_fnexpr
+    return fnexpr, def_fnexpr, aux_fnexpr
 end
 
 
-function _preamble_body(fnbody, fnargs)
-
-    # Unfolds the body to the expresion graph (AST) of the function,
-    # a priori as unary and binary calls
-    fnbody = sanitize(to_expr( ExGraph(fnbody) ))
-
-    v_vars = Symbol[]            # List of symbols with created variables
-    v_aux = Dict{Symbol,Expr}()  # Auxiliary functions
-    v_assign = Tuple{Int,Expr}[] # Numeric assignements (to be deleated)
-
-    # Populate v_vars, v_assign, v_aux
-    for (i,aa) in enumerate(fnbody.args)
-        aavar = aa.args[1]
-        if isa(aa.args[2], Expr)
-            aaa = aa.args[2]
-            fnexpr, auxfnexpr = _replace_exprs!(aaa, aavar, v_vars)
-            fnbody.args[i] = fnexpr
-            if auxfnexpr.head != :nothing
-                newvar = v_vars[end]
-                push!(v_aux, auxfnexpr.args[1] => auxfnexpr.args[2])
-            end
-        elseif isa(aa.args[2], Number)
-            push!(v_assign, (i,aa))
-        else
-        end
-    end
-
-    # Clean-up numeric assignements
-    for kk in reverse(eachindex(v_assign))
-        deleteat!(fnbody.args, v_assign[kk][1])
-        fnbody = subs(fnbody,
-            Dict(v_assign[kk][2].args[1] => v_assign[kk][2].args[2]))
-    end
-
-    # Define temporary variables
+function _nfnpreamble(v_vars, v_preamb, fnargs)
     preamble = Expr(:block,)
     for i in eachindex(v_vars)
         nv = v_vars[i]
-        if in(nv, keys(v_aux))
-            push!(preamble.args, parse("$nv = Taylor1( $(v_aux[nv]) , order)") )
+        if in(nv, keys(v_preamb))
+            push!(preamble.args, parse("$nv = Taylor1( $(v_preamb[nv]) , order)") )
             continue
+        else
+            # is this unreachable ?
+            push!(preamble.args, parse("$nv = zero( $(fnargs[2]) )") )
         end
-        push!(preamble.args, parse("$nv = zero( $(fnargs[2]) )") )
     end
-
-    return preamble, fnbody
+    return preamble
 end
 
+
+function _nfnpreamble_body(fnbody, fnargs)
+
+    # Unfolds the body to the expresion graph (AST) of the function,
+    # a priori as unary and binary calls
+    newfnbody = sanitize(to_expr( ExGraph(fnbody) ))
+
+    v_vars = Symbol[]            # List of symbols with created variables
+    v_preamb = Dict{Symbol,Expr}()  # Auxiliary definitions
+    v_assign = Tuple{Int,Expr}[] # Numeric assignements (to be deleated)
+
+    # Populate v_vars, v_assign, v_preamb
+    for (i,aa) in enumerate(newfnbody.args)
+        aavar = aa.args[1]
+        aaa = aa.args[2]
+        if isa(aaa, Expr)
+            push!(v_vars, aavar)
+            fnexpr, def_fnexpr, auxfnexpr = _replacevars!(aaa, aavar, v_vars)
+            push!(v_preamb, def_fnexpr.args[1] => def_fnexpr.args[2])
+            newfnbody.args[i] = fnexpr
+            if auxfnexpr.head != :nothing
+                newvar = v_vars[end]
+                push!(v_preamb, auxfnexpr.args[1] => auxfnexpr.args[2])
+            end
+        elseif isa(aaa, Number)
+            push!(v_assign, (i, aa))
+        else #needed?
+            @show(aa.args[2], typeof(aa.args[2]))
+            error("Different from `Number` or `Expr`")
+        end
+    end
+
+    @show(v_vars, v_preamb, v_assign)
+    println()
+    @show(newfnbody)
+    println()
+
+    # Clean-up numeric assignements in body
+    for kk in reverse(v_assign)
+        newfnbody = subs(newfnbody, Dict(kk[2].args[1] => kk[2].args[2]))
+        deleteat!(newfnbody.args, kk[1])
+    end
+
+    @show(newfnbody)
+    println()
+
+    # Define premable (temporary allocations)
+    preamble = _nfnpreamble(v_vars, v_preamb, fnargs)
+
+    return preamble, newfnbody
+end
 
 
 function to_parsed_jetcoeffs( ex )
@@ -174,15 +186,15 @@ function to_parsed_jetcoeffs( ex )
         throw(ArgumentError("Must be a function call\n", ex))
 
     # Set up new function
-    newfunction = _head_newfunct(fn, fnargs)
+    newfunction = _nfnhead(fn, fnargs)
 
-    # Block for th for-loop; it will include parsed body
-    forloopblock = copy(_LOOP_PARSEDFN_SCALAR)
+    # for-loop block; it will include parsed body
+    forloopblock = copy(_LOOP_PARSEDFN)
     forloopblock = subs(forloopblock,
         Dict(:(ordnext = ord + 1) => Expr(:block, :(ordnext = ord + 1))) )
 
     # Transform graph representation of the body of the function
-    preamble, fnbody = _preamble_body(fnbody, fnargs);
+    preamble, fnbody = _nfnpreamble_body(fnbody, fnargs);
 
     #### FALTA INCLUIR LA RECURRENCIA EN LAS ECS!!
 
@@ -211,5 +223,6 @@ function to_parsed_jetcoeffs( ex )
         newfunction = subs(newfunction, Dict(fnargs[3] => :(__dx)))
     end
 
-    newfunction
+    @show(newfunction)
+    nothing
 end
