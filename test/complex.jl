449076c60b0f4a6602b79a41e6ea53f96b69fe1c
Jorge Pérez <PerezHz@users.noreply.github.com>
2017-08-26 18:03:59 -0500

    Add Taylor1{Taylor1{T}} integration methods; improve testing coverage (#34)
    * Add Taylor1{Taylor1{T}} integration methods
    
    * Add Taylor1 jet transport tests (more to be added)
    
    * Add more tests
    
    * Add more more tests
    
    * Attempt at fixing failing appveyor tests
    
    * Add test case: Taylor1 jet transport (t0,tmax): harmonic oscillator
    
    * Add more tests
    
    * Improve coverage of complex dep vars tests
    
    * Improve testing coverage of liapunovspectrum.jl
    
    * Fix classicalGS! tests, add .codecov.yml
    
    * Fix test name
    
    * Add missing tests (complex, TaylorN jet transport)
    
    * Add missing tests for complex case

--- test/complex.jl
+++ test/complex.jl
@@ -17,13 +17,21 @@ end
 @testset "Test integration of ODE with complex dependent variables (1)" begin
     z0 = complex(0.0, 1.0)
     tr = 0.0:pi/8:2pi
-
-    zsol1 = taylorinteg(eqs1, z0, tr, 28, 1.0e-20)
+    ts = 0.0:pi:2pi
+    zsol1 = taylorinteg(eqs1, z0, ts, _order, _abstol, maxsteps=1)
+    # @test length(zsol1) == length(tr)
+    @test size(zsol1) == (length(ts),)
+    zsol1 = taylorinteg(eqs1, z0, tr, _order, _abstol, maxsteps=1)
+    @test length(zsol1) == length(tr)
+    zsol1 = taylorinteg(eqs1, z0, tr, _order, _abstol)
     @test zsol1[1] == z0
     @test isapprox( zsol1[2]  , z0*exp(-tr[2]) )
     @test isapprox( zsol1[6], z0*exp(-tr[6]) )
     @test isapprox( zsol1[end], z0*exp(-tr[end]) )
-    tt, zsol1 = taylorinteg(eqs1, z0, 0.0, 2pi, 28, 1.0e-20)
+    tt, zsol1 = taylorinteg(eqs1, z0, 0.0, 2pi, _order, _abstol, maxsteps=1)
+    @test length(tt) == 2
+    @test length(zsol1) == 2
+    tt, zsol1 = taylorinteg(eqs1, z0, 0.0, 2pi, _order, _abstol)
     @test zsol1[1] == z0
     @test isapprox( zsol1[2]  , z0*exp(-tt[2]) )
     @test isapprox( zsol1[end], z0*exp(-tt[end]) )
@@ -32,12 +40,19 @@ end
 @testset "Test integration of ODE with complex dependent variables (2)" begin
     z0 = complex(0.0, 1.0)
     tr = 0.0:pi/8:2pi
-
-    zsol2 = taylorinteg(eqs2, z0, tr, 28, 1.0e-20)
+    ts = 0.0:pi:2pi
+    zsol2 = taylorinteg(eqs2, z0, ts, _order, _abstol, maxsteps=1)
+    @test size(zsol2) == (length(ts),)
+    zsol2 = taylorinteg(eqs2, z0, tr, _order, _abstol, maxsteps=1)
+    @test length(zsol2) == length(tr)
+    zsol2 = taylorinteg(eqs2, z0, tr, _order, _abstol)
     @test zsol2[1] == z0
     @test isapprox( zsol2[3], z0*exp( complex(0.0, tr[3])) )
     @test isapprox( zsol2[5], z0*exp( complex(0.0, tr[5])) )
-    tt, zsol2 = taylorinteg(eqs2, z0, 0.0, 2pi, 28, 1.0e-20)
+    tt, zsol2 = taylorinteg(eqs2, z0, 0.0, 2pi, _order, _abstol, maxsteps=1)
+    @test length(tt) == 2
+    @test length(zsol2) == 2
+    tt, zsol2 = taylorinteg(eqs2, z0, 0.0, 2pi, _order, _abstol)
     @test zsol2[1] == z0
     @test isapprox( zsol2[2], z0*exp( complex(0.0, tt[2])) )
     @test isapprox( zsol2[end], z0*exp( complex(0.0, tt[end])) )
@@ -45,14 +60,22 @@ end
 
 @testset "Test integration of ODE with complex dependent variables (3)" begin
     z0 = complex(0.0, 1.0)
+    zz0 = [z0, z0]
     tr = 0.0:pi/8:2pi
-
-    zsol3 = taylorinteg(eqs3!, [z0, z0], tr, 28, 1.0e-20)
+    ts = 0.0:pi:2pi
+    zsol3 = taylorinteg(eqs3!, zz0, ts, _order, _abstol, maxsteps=1)
+    @test size(zsol3) == (length(ts), length(zz0))
+    zsol3 = taylorinteg(eqs3!, zz0, tr, _order, _abstol, maxsteps=1)
+    @test size(zsol3) == (length(tr), length(zz0))
+    zsol3 = taylorinteg(eqs3!, [z0, z0], tr, _order, _abstol)
     @test isapprox( zsol3[4,1], z0*exp(-tr[4]) )
     @test isapprox( zsol3[7,1], z0*exp(-tr[7]) )
     @test isapprox( zsol3[4,2], z0*exp( complex(0.0, tr[4])) )
     @test isapprox( zsol3[7,2], z0*exp( complex(0.0, tr[7])) )
-    tt, zsol3 = taylorinteg(eqs3!, [z0,z0], 0.0, 2pi, 28, 1.0e-20)
+    tt, zsol3 = taylorinteg(eqs3!, zz0, 0.0, 2pi, _order, _abstol, maxsteps=1)
+    @test length(tt) == 2
+    @test size(zsol3) == (length(tt), length(zz0))
+    tt, zsol3 = taylorinteg(eqs3!, zz0, 0.0, 2pi, _order, _abstol)
     @test zsol3[1,1] == z0
     @test zsol3[1,2] == z0
     @test isapprox( zsol3[2,1], z0*exp( -tt[2]) )
