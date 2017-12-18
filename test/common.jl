b736dd6ec4fe473dda4d2c6b4fc5c4e306de4383
Christopher Rackauckas <accounts@chrisrackauckas.com>
2017-12-18 09:24:29 -0800

    Update common.jl

--- test/common.jl
+++ test/common.jl
@@ -5,7 +5,7 @@ using TaylorIntegration, Base.Test
     u0 = 0.5
     tspan = (0.0,1.0)
     prob = ODEProblem(f,u0,tspan)
-    sol = solve(prob,TaylorMethod(50))
+    sol = solve(prob,TaylorMethod(50),abstol=1e-20)
 
     @test sol[end] - 0.5exp(1) < 1e-12
 end
@@ -15,7 +15,7 @@ end
     u0 = rand(4,2)
     tspan = (0.0,1.0)
     prob = ODEProblem(f,u0,tspan)
-    sol = solve(prob,TaylorMethod(50))
+    sol = solve(prob,TaylorMethod(50),abstol=1e-20)
 
     @test norm(sol[end] - u0.*exp(1)) < 1e-12
 end
