04968359b3600f339d58c22e05641e6e83d87ad6
Jorge Pérez <jperez@icf.unam.mx>
2018-09-07 16:13:48 -0500

    Fix existing Lyapunov tests

--- test/lyapunov.jl
+++ test/lyapunov.jl
@@ -7,14 +7,20 @@ using Test
 const _order = 28
 const _abstol = 1.0E-20
 
+#Lorenz system parameters
+const σ = 16.0
+const β = 4.0
+const ρ = 45.92
+
+#Lorenz system ODE:
+function lorenz!(t, x, dx)
+    dx[1] = σ*(x[2]-x[1])
+    dx[2] = x[1]*(ρ-x[3])-x[2]
+    dx[3] = x[1]*x[2]-β*x[3]
+    nothing
+end
+
 @testset "Test `stabilitymatrix!`" begin
-    function lorenz!(t, x, dx) #Lorenz system ODE:
-        dx[1] = σ*(x[2]-x[1])
-        dx[2] = x[1]*(ρ-x[3])-x[2]
-        dx[3] = x[1]*x[2]-β*x[3]
-        nothing
-    end
-    σ = 16.0; β = 4.0; ρ = 45.92 #Lorenz system parameters
     t0 = rand() #the initial time
     xi = set_variables("δ", order=1, numvars=3)
     t_ = Taylor1([t0,1],_order)
@@ -58,19 +64,6 @@ end
     t0 = 0.0 #the initial time
     tmax = t0+20.0 #final time of integration
 
-    #Lorenz system ODE:
-    function lorenz!(t, x, dx)
-        dx[1] = σ*(x[2]-x[1])
-        dx[2] = x[1]*(ρ-x[3])-x[2]
-        dx[3] = x[1]*x[2]-β*x[3]
-        nothing
-    end
-
-    #Lorenz system parameters
-    σ = 16.0
-    β = 4.0
-    ρ = 45.92
-
     #Calculate trace of Lorenz system Jacobian via TaylorSeries.jacobian:
     xi = set_variables("δ", order=1, numvars=length(x0))
     x0TN = [ x0[1]+xi[1], x0[2]+xi[2], x0[3]+xi[3] ]
@@ -110,27 +103,15 @@ end
     t0 = 0.0 #the initial time
     tmax = t0+20.0 #final time of integration
 
-    #Lorenz system ODE:
-    function lorenz!(t, x, dx)
-        dx[1] = σ*(x[2]-x[1])
-        dx[2] = x[1]*(ρ-x[3])-x[2]
-        dx[3] = x[1]*x[2]-β*x[3]
-        nothing
-    end
-
-    #Lorenz system parameters
-    σ = 16.0
-    β = 4.0
-    ρ = 45.92
-
     #Calculate trace of Lorenz system Jacobian via TaylorSeries.jacobian:
     xi = set_variables("δ", order=1, numvars=length(x0))
-    x0TN = [ x0[1]+xi[1], x0[2]+xi[2], x0[3]+xi[3] ]
+    x0TN = x0+xi
+    # x0TN = [ x0[1]+xi[1], x0[2]+xi[2], x0[3]+xi[3] ]
     dx0TN = similar(x0TN)
     lorenz!(t0, x0TN, dx0TN)
     lorenztr = tr(jacobian(dx0TN)) #trace of Lorenz system Jacobian matrix
 
-    @test lorenztr == -(σ+one(Float64)+β)
+    @test lorenztr == -(1+σ+β)
 
     xw, λw = lyap_taylorinteg(lorenz!, x0, t0:1.0:tmax, 28, _abstol; maxsteps=2)
 
