fc46f9f66ca39a36798f555a1bffb5af4754fae2
Luis Benet <lbenet@users.noreply.github.com>
2019-06-19 18:08:03 -0500

    Regression in stepsize (#75)
    * Fix regression in stepsize (branching and type instability)
    
    * Add docstrings; rename _finite_stepsize -> _second_stepsize
    
    * Fix a small typo

--- src/explicitode.jl
+++ src/explicitode.jl
@@ -112,48 +112,80 @@ Returns a maximum time-step for a the Taylor expansion `x`
 using a prescribed absolute tolerance `epsilon` and the last two
 Taylor coefficients of (each component of) `x`.
 
-Note that `x` is of type `Taylor1{T}` or `Vector{Taylor1{T}}`, including
-also the cases `Taylor1{TaylorN{T}}` and `Vector{Taylor1{TaylorN{T}}}`.
+Note that `x` is of type `Taylor1{U}` or `Vector{Taylor1{U}}`, including
+also the cases `Taylor1{TaylorN{U}}` and `Vector{Taylor1{TaylorN{U}}}`.
 
+Depending of `eltype(x)`, i.e., `U<:Number`, it may be necessary to overload
+`stepsize`, specializing it on the type `U`, to avoid type instabilities.
 """
 function stepsize(x::Taylor1{U}, epsilon::T) where {T<:Real, U<:Number}
+    R = promote_type(typeof(norm(constant_term(x), Inf)), T)
     ord = x.order
-    h = convert(T, Inf)
+    h = convert(R, Inf)
+    z = zero(R)
     for k in (ord-1, ord)
         @inbounds aux = norm( x[k], Inf)
-        aux == zero(T) && continue
-        aux = epsilon / aux
-        kinv = one(T)/k
-        aux = aux^kinv
-        h = min(h, aux)
+        aux == z && continue
+        aux1 = _stepsize(aux, epsilon, k)
+        h = min(h, aux1)
     end
-    isfinite(h) && return h
-
-    # If isinf(h) == true, we use the maximum (finite)
-    # step-size obtained from all coefficients as above.
-    # Note that the time step is independent from `epsilon`
-    hh = zero(T)
-    for k in 1:ord-1
-        @inbounds aux = norm( x[k], Inf)
-        aux == zero(T) && continue
-        aux = 1 / aux
-        kinv = one(T)/k
-        aux = aux^kinv
-        hh = max(hh, aux)
-    end
-    hh == zero(T) && return h
-
-    return hh
+    return h::R
 end
 
 function stepsize(q::AbstractArray{Taylor1{U},1}, epsilon::T) where
         {T<:Real, U<:Number}
-    h = convert(T, Inf)
+    R = promote_type(typeof(norm(constant_term(q[1]), Inf)), T)
+    h = convert(R, Inf)
     for i in eachindex(q)
         @inbounds hi = stepsize( q[i], epsilon )
         h = min( h, hi )
     end
-    return h
+
+    # If `isinf(h)==true`, we use the maximum (finite)
+    # step-size obtained from all coefficients as above.
+    # Note that the time step is independent from `epsilon`.
+    if isinf(h)
+        h = zero(R)
+        for i in eachindex(q)
+            @inbounds hi = _second_stepsize(q[i], epsilon)
+            h = max( h, hi )
+        end
+    end
+    return h::R
+end
+
+"""
+    _stepsize(aux1, epsilon, k)
+
+Helper function to avoid code repetition.
+Returns ``(epsilon/aux1)^(1/k)``.
+"""
+@inline function _stepsize(aux1::U, epsilon::T, k::Int) where {T<:Real, U<:Number}
+    aux = epsilon / aux1
+    kinv = 1 / k
+    return aux^kinv
+end
+
+"""
+    _second_stepsize(x, epsilon)
+
+Corresponds to the "second stepsize control" in Jorba and Zou
+(2005) paper. We use it if [`stepsize`](@ref) returns `Inf`.
+"""
+function _second_stepsize(x::Taylor1{U}, epsilon::T) where {T<:Real, U<:Number}
+    R = promote_type(typeof(norm(constant_term(x), Inf)), T)
+    x == zero(x) && return convert(R, Inf)
+    ord = x.order
+    z = zero(R)
+    u = one(R)
+    h = z
+    for k in 1:ord-2
+        @inbounds aux = norm( x[k], Inf)
+        aux == z && continue
+        aux1 = _stepsize(aux, u, k)
+        h = max(h, aux1)
+    end
+    return h::R
 end
 
 #taylorstep
@@ -193,6 +225,9 @@ function taylorstep!(f, t::Taylor1{T}, x::Taylor1{U}, t0::T, t1::T, order::Int,
 
     # Compute the step-size of the integration using `abstol`
     δt = stepsize(x, abstol)
+    if isinf(δt)
+        δt = _second_stepsize(x, abstol)
+    end
     δt = min(δt, t1-t0)
 
     return δt
