341c12413c332bcf71089cc1d86ba6edd6eef215
agerlach <599421+agerlach@users.noreply.github.com>
2019-10-03 15:21:31 -0400

    add support for saveat expansion (#78)
    * add support for saveat expansion
    
    * remove :saveat warning
    
    * Add test for saveat usage in the common interface
    
    * Update saveat interface to mimic OrdinaryDiffEq behavior. Added OrdinaryDiffEq dependency but was able to use a function there to ensure consistency. Additional tests added for various saveat conditions and for inplace and outofplace functions.
    
    * Removed Manifest and updated targets

--- src/common.jl
+++ src/common.jl
@@ -1,6 +1,7 @@
 using DiffEqBase
 
 import DiffEqBase: ODEProblem, solve
+import OrdinaryDiffEq: tstop_saveat_disc_handling
 
 const warnkeywords =
     (:save_idxs, :d_discontinuities, :unstable_check, :save_everystep,
@@ -8,7 +9,7 @@ const warnkeywords =
      :dtmin, :force_dtmin, :internalnorm, :gamma, :beta1, :beta2,
      :qmax, :qmin, :qsteady_min, :qsteady_max, :qoldinit, :failfactor,
      :maxiters, :isoutofdomain, :unstable_check,
-     :calck, :progress, :timeseries_steps, :tstops, :saveat, :dense)
+     :calck, :progress, :timeseries_steps, :tstops, :dense)
 
 global warnlist = Set(warnkeywords)
 
@@ -24,13 +25,19 @@ TaylorMethod() = error("Maximum order must be specified for the Taylor method")
 export TaylorMethod
 
 function DiffEqBase.solve(
-    prob::DiffEqBase.AbstractODEProblem{uType,tType,isinplace},
+    prob::DiffEqBase.AbstractODEProblem{uType,tupType,isinplace},
     alg::AlgType,
     timeseries=[],ts=[],ks=[];
-    verbose=true, abstol = 1e-6, save_start = true,
+    verbose=true,
+    saveat=eltype(tupType)[],
+    abstol = 1e-6,
+    save_start =  isempty(saveat) || saveat isa Number || prob.tspan[1] in saveat,
+    save_end   =  isempty(saveat) || saveat isa Number || prob.tspan[2] in saveat,
     timeseries_errors=true, maxiters = 1_000_000,
-    kwargs...) where
-        {uType, tType, isinplace, AlgType <: TaylorAlgorithm}
+    callback=nothing, kwargs...) where
+        {uType, tupType, isinplace, AlgType <: TaylorAlgorithm}
+
+    tType = eltype(tupType)
 
     if verbose
         warned = !isempty(kwargs) && check_keywords(alg, kwargs, warnlist)
@@ -44,34 +51,69 @@ function DiffEqBase.solve(
     sizeu = size(prob.u0)
     f = prob.f
 
+    tspan = prob.tspan
+
+    _, saveat_vec_, _ = tstop_saveat_disc_handling(1,saveat,1,tspan)
+    saveat_vec = sort(saveat_vec_.valtree)
+
+    if !isempty(saveat) && save_end && saveat_vec[end] != tspan[2]
+        push!(saveat_vec,tspan[2])
+    end
+    if !isempty(saveat) && saveat_vec[1] != tspan[1]
+        prepend!(saveat_vec,tspan[1])
+    end
+
     if !isinplace && typeof(prob.u0) <: Vector{Float64}
         f! = (du, u, p, t) -> (du .= f(u, p, t); 0)
-        t, vectimeseries = taylorinteg(f!, prob.u0, prob.tspan[1], prob.tspan[2],
-            alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        if isempty(saveat_vec)
+            t, vectimeseries = taylorinteg(f!, prob.u0, prob.tspan[1], prob.tspan[2],
+                alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        else
+            t = saveat_vec
+            vectimeseries = taylorinteg(f!, prob.u0, t,
+                alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        end
 
     elseif !isinplace && typeof(prob.u0) <: AbstractArray
         f! = (du, u, p, t) -> (du .= vec(f(reshape(u, sizeu), p, t)); 0)
-        t, vectimeseries = taylorinteg(f!, prob.u0, prob.tspan[1], prob.tspan[2],
-            alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        if isempty(saveat_vec)
+            t, vectimeseries = taylorinteg(f!, prob.u0, prob.tspan[1], prob.tspan[2],
+                alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        else
+            t = saveat_vec
+            vectimeseries = taylorinteg(f!, prob.u0, t,
+                alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        end
 
     elseif isinplace && typeof(prob.u0) <: AbstractArray{eltype(uType),2}
         f! = (du, u, p, t) -> (
                 dd = reshape(du, sizeu); uu = reshape(u, sizeu);
                 f(dd, uu, p, t); u = vec(uu); du = vec(dd); 0)
         u0 = vec(prob.u0)
-        t, vectimeseries = taylorinteg(f!, u0, prob.tspan[1], prob.tspan[2],
-            alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
-
+        if isempty(saveat_vec)
+            t, vectimeseries = taylorinteg(f!, u0, prob.tspan[1], prob.tspan[2],
+                alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        else
+            t = saveat_vec
+            vectimeseries = taylorinteg(f!, prob.u0, t,
+                alg.order, abstol, prob.p, maxsteps=maxiters, parse_eqs=false)
+        end
     else
         if haskey(kwargs, :parse_eqs)
             parse_eqs = kwargs[:parse_eqs]
         else
             parse_eqs = true
         end
-        t, vectimeseries = taylorinteg(prob.f.f, prob.u0, prob.tspan[1],
-            prob.tspan[2], alg.order, abstol, prob.p, maxsteps=maxiters,
-            parse_eqs=parse_eqs)
-
+        if isempty(saveat_vec)
+            t, vectimeseries = taylorinteg(f, prob.u0, prob.tspan[1],
+                prob.tspan[2], alg.order, abstol, prob.p, maxsteps=maxiters,
+                parse_eqs=parse_eqs)
+        else
+            t = saveat_vec
+            vectimeseries = taylorinteg(f, prob.u0, t,
+                alg.order, abstol, prob.p, maxsteps=maxiters,
+                parse_eqs=parse_eqs)
+        end
     end
 
     if save_start
@@ -88,7 +130,7 @@ function DiffEqBase.solve(
           push!(_timeseries, reshape(view(vectimeseries, i, :, )', sizeu))
       end
     else
-      _timeseries = vec(vectimeseries)
+      _timeseries = vec(vectimeseries)[start_idx:end]
     end
 
     DiffEqBase.build_solution(prob,  alg, _t, _timeseries,
--- test/common.jl
+++ test/common.jl
@@ -1,4 +1,4 @@
-using TaylorIntegration, Test, DiffEqBase
+using TaylorIntegration, Test, DiffEqBase, OrdinaryDiffEq
 using LinearAlgebra: norm
 
 f(u,p,t) = u
@@ -11,12 +11,46 @@ f(u,p,t) = u
     @test sol[end] - 0.5*exp(1) < 1e-12
 end
 
-f(du, u, p, t) = (du .= u)
+f!(du, u, p, t) = (du .= u)
 @testset "Test integration of ODE with abstract arrays in common interface" begin
     u0 = rand(4, 2)
     tspan = (0.0, 1.0)
-    prob = ODEProblem(f, u0, tspan)
+    prob = ODEProblem(f!, u0, tspan)
     sol = solve(prob, TaylorMethod(50), abstol=1e-20)
 
     @test norm(sol[end] - u0.*exp(1)) < 1e-12
+end
+
+u0 = 1.0
+tspan = (0.0,5.0)
+saveat_inputs = ([], 0:1:tspan[2], 0:1:(tspan[2]+5), 3:1:tspan[2], collect(0:1:tspan[2]))
+@testset "Test saveat behavior with numbers in common interface" begin
+    prob = ODEProblem(f, u0, tspan)
+    for s ∈ saveat_inputs
+        if isempty(s)
+            sol = solve(prob, TaylorMethod(20),abstol=1e-20)
+        else
+            sol = solve(prob, Tsit5(),abstol=1e-20, saveat = s)
+        end
+        sol_taylor = solve(prob, TaylorMethod(20),abstol=1e-20, saveat = s)
+
+        @test all(sol.t .== sol_taylor.t)
+        @test length(sol_taylor.t) == length(sol_taylor.u)
+    end
+end
+
+u0 = rand(2)
+@testset "Test saveat behavior with abstract arrays in common interface" begin
+    prob = ODEProblem(f!, u0, tspan)
+    for s ∈ saveat_inputs
+        if isempty(s)
+            sol = solve(prob, TaylorMethod(20),abstol=1e-20)
+        else
+            sol = solve(prob, Tsit5(),abstol=1e-20, saveat = s)
+        end
+        sol_taylor = solve(prob, TaylorMethod(20),abstol=1e-20, saveat = s)
+
+        @test all(sol.t .== sol_taylor.t)
+        @test length(sol_taylor.t) == length(sol_taylor.u)
+    end
 end
