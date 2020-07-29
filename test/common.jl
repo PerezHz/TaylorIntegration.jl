fa62fc4e6dca1e1a1d13c5f465c05fea16a37810
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2020-07-29 16:23:46 +0300

    Add tests for DynamicalODEs

--- test/common.jl
+++ test/common.jl
@@ -1,5 +1,6 @@
 using TaylorIntegration, Test, DiffEqBase
 using LinearAlgebra: norm
+using StaticArrays
 
 @testset "Testing `common.jl`" begin
 
@@ -75,5 +76,51 @@ using LinearAlgebra: norm
         # Using a `callback`
         prob2 = ODEProblem(f!, u0, tspan, callback=nothing)
         @test_throws ErrorException solve(prob2, TaylorMethod(10), abstol=1e-20)
+    end
+
+    @testset "Test integration of DynamicalODEPoblem" begin
+        function iip_q̇(dq,p,q,params,t)
+            dq[1] = p[1]
+            dq[2] = p[2]
+        end
+
+        function iip_ṗ(dp,p,q,params,t)
+            dp[1] = -q[1] * (1 + 2q[2])
+            dp[2] = -q[2] - (q[1]^2 - q[2]^2)
+        end
+
+        iip_q0 = [0.1, 0.]
+        iip_p0 = [0., 0.5]
+
+
+        function oop_q̇(p, q, params, t)
+            p
+        end
+
+        function oop_ṗ(p, q, params, t)
+            dp1 = -q[1] * (1 + 2q[2])
+            dp2 = -q[2] - (q[1]^2 - q[2]^2)
+            @SVector [dp1, dp2]
+        end
+
+        oop_q0 = @SVector [0.1, 0.]
+        oop_p0 = @SVector [0., 0.5]
+
+        T(p) = 1//2 * norm(p)^2
+        V(q) = 1//2 * (q[1]^2 + q[2]^2 + 2q[1]^2 * q[2]- 2//3 * q[2]^3)
+        H(p,q, params) = T(p) + V(q)
+
+        E = H(iip_p0, iip_q0, nothing)
+
+        energy_err(sol) = maximum(i->H([sol[1,i], sol[2,i]], [sol[3,i], sol[4,i]], nothing)-E, 1:length(sol.u))
+
+        iip_prob = DynamicalODEProblem(iip_ṗ, iip_q̇, iip_p0, iip_q0, (0., 100.))
+        oop_prob = DynamicalODEProblem(oop_ṗ, oop_q̇, oop_p0, oop_q0, (0., 100.))
+
+        sol1 = solve(iip_prob, TaylorMethod(50), abstol=1e-20)
+        @test energy_err(sol1) < 1e-10
+
+        sol2 = solve(oop_prob, TaylorMethod(50), abstol=1e-20)
+        @test energy_err(sol2) < 1e-10
     end
 end
