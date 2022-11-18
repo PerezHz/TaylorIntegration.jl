880f1e0a21e77764d9f68dd18ecc25b21f20f664
Luis Benet <benet@icf.unam.mx>
2022-11-18 17:12:23 -0600

    Add date to output filenames, and Dates.jl to Project.toml

--- run_benchmarks.jl
+++ run_benchmarks.jl
@@ -1,6 +1,7 @@
-using PkgBenchmark
+using PkgBenchmark, Dates
 
 const PB = PkgBenchmark
+const directorypath = dirname(@__FILE__)
 
 # ==========
 # Run PkgBenchmark `judge`
@@ -11,11 +12,12 @@ compara = judge("TaylorIntegration", "test/benchmarking", "test/v0.8.12")
 # ==========
 # Save results
 # ==========
-comparapath = joinpath(dirname(@__FILE__), "benchmark/comparacion.md")
+hoy = string(today())
+comparapath = joinpath(directorypath, "benchmark", hoy * "_comparacion.md")
 export_markdown(comparapath, compara, export_invariants=true)
 #
-comparapath = joinpath(dirname(@__FILE__), "benchmark/resultadosT.json")
+comparapath = joinpath(directorypath, "benchmark", hoy * "_resultadosTarg.json")
 PB.writeresults(comparapath, PB.target_result(compara))
 #
-comparapath = joinpath(dirname(@__FILE__), "benchmark/resultadosB.json")
+comparapath = joinpath(directorypath, "benchmark", hoy * "_resultadosBase.json")
 PB.writeresults(comparapath, PB.baseline_result(compara))
