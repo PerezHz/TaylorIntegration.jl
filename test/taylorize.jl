1a491d83161b67d8aa736a314b2f7725b3ce3d6b
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2024-02-10 21:56:16 +0100

    Avoid use of zeroq1 workaround in JT test

--- test/taylorize.jl
+++ test/taylorize.jl
@@ -1174,8 +1174,7 @@ import Logging: Warn
         @test norm(xv_jt[end,:]-xv[end,:]) < 20eps(norm(xv[end,:]))
 
         @taylorize function kepler1!(dq, q, p, t)
-            local zeroq1 = zero(q[1])
-            local μ = -1.1 + zeroq1
+            local μ = -1.0
             x1s = q[3]
             y1s = q[4]
             r_p2 = ((x1s^2)+(y1s^2))
