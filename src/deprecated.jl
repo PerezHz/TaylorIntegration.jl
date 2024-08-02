3320603a84056dc51745db32adc7689e2d465835
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2024-08-02 19:34:45 +0200

    Add deprecation message

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -16,7 +16,7 @@ function taylorinteg_old(f, g, x0, t0, tmax, order, abstol, params; kwargs...)
     end
 end
 
-Base.@deprecate taylorinteg(f, x0, t0, tmax, order, abstol, ::Val{false}, params; maxsteps=500, parse_eqs=true) taylorinteg_old(f, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=false)
-Base.@deprecate taylorinteg(f, x0, t0, tmax, order, abstol, ::Val{true}, params; maxsteps=500, parse_eqs=true) taylorinteg_old(f, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=true)
-Base.@deprecate taylorinteg(f, g, x0, t0, tmax, order, abstol, ::Val{false}, params; maxsteps=500, parse_eqs=true, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0))) taylorinteg_old(f, g, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=false, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0)))
-Base.@deprecate taylorinteg(f, g, x0, t0, tmax, order, abstol, ::Val{true}, params; maxsteps=500, parse_eqs=true, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0))) taylorinteg_old(f, g, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=true, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0)))
+Base.@deprecate taylorinteg(f, x0, t0::Real, tmax, order, abstol, ::Val{false}, params; maxsteps=500, parse_eqs=true) taylorinteg_old(f, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=false)
+Base.@deprecate taylorinteg(f, x0, t0::Real, tmax, order, abstol, ::Val{true}, params; maxsteps=500, parse_eqs=true) taylorinteg_old(f, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=true)
+Base.@deprecate taylorinteg(f, g::Function, x0, t0, tmax, order, abstol, ::Val{false}, params; maxsteps=500, parse_eqs=true, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0))) taylorinteg_old(f, g, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=false, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0)))
+Base.@deprecate taylorinteg(f, g::Function, x0, t0, tmax, order, abstol, ::Val{true}, params; maxsteps=500, parse_eqs=true, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0))) taylorinteg_old(f, g, x0, t0, tmax, order, abstol, params; maxsteps=500, parse_eqs=true, dense=true, eventorder=0, newtoniter=10, nrabstol=eps(typeof(t0)))
