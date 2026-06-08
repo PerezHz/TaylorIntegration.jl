45532ab2db83d28b1c10009aec0eaa98ed454935
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2026-06-07 23:33:24 -0400

    Fix

--- src/rootfinding.jl
+++ src/rootfinding.jl
@@ -381,8 +381,8 @@ function taylorinteg!(
 
     x_dx = vcat(x, dx)
     g_dg = vcat(g_tupl[2], g_tupl_old[2])
-    x_dx_val = evaluate(x_dx)
-    g_dg_val = vcat(evaluate(g_tupl[2]), evaluate(g_tupl_old[2]))
+    x_dx_val = _stored_state(evaluate(x_dx))
+    g_dg_val = _stored_state(vcat(evaluate(g_tupl[2]), evaluate(g_tupl_old[2])))
 
     tvS = Array{U}(undef, maxsteps + 1)
     xvS = similar(xv)
@@ -517,8 +517,8 @@ function taylorinteg!(
 
     x_dx = vcat(x, dx)
     g_dg = vcat(g_tupl[2], g_tupl_old[2])
-    x_dx_val = evaluate(x_dx)
-    g_dg_val = vcat(evaluate(g_tupl[2]), evaluate(g_tupl_old[2]))
+    x_dx_val = _stored_state(evaluate(x_dx))
+    g_dg_val = _stored_state(vcat(evaluate(g_tupl[2]), evaluate(g_tupl_old[2])))
 
     tvS = Array{U}(undef, maxsteps + 1)
     xvS = similar(xv)
